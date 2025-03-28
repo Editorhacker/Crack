c5d9fad45984e78318c34dbbd7a51fc29f61eadb
Editorhacker <94226999+Editorhacker@users.noreply.github.com>
2025-03-28 17:05:25 +0530

    Update Express.js

--- Express.js
+++ Express.js
@@ -14,6 +14,7 @@ mongoose.connect("mongodb+srv://Crack:Crack@crackit.sqvqpyt.mongodb.net/", {
   .catch(err => console.log(err));
 
 const UserSchema = new mongoose.Schema({
+    name : String,
     email: String,
     password: String
 });
@@ -39,7 +40,7 @@ app.post("/register", async (req, res) => {
         return res.status(400).json({ message: "User already exists" });
     }
 
-    const newUser = new User({ email, password });
+    const newUser = new User({ name, email, password });
     await newUser.save();
     res.status(201).json({ message: "User Registered Successfully!" });
 });
